cd7486db5db46fd3a9c0ffcd9a49a08f6e17fde3
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-01-28 23:16:58 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,4 @@
 
 terraform {
-  # Use "terraform init -backend-config=PATH" to setup state in a s3 bucket
-  # backend "s3" {}
-  # DO Spaces
-  # local for now
-  backend "local" {}
+  backend "s3" {}
 }
