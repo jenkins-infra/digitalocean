838a5422bb490d9025329696bfa6f18a3543bd36
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-04-01 17:01:54 +0200

    fix: hardcode k8s version until the switch to 1.21 (#24)
    doctl kubernetes options versions doesn't return anything for 1.20.x anymore.
    Ref: https://github.com/jenkins-infra/helpdesk/issues/2861

--- locals.tf
+++ locals.tf
@@ -5,6 +5,9 @@ resource "random_string" "suffix" {
 
 locals {
   cluster_name           = lower("jenkins-infra-doks-${random_string.suffix.result}")
-  doks_version           = data.digitalocean_kubernetes_versions.k8s.latest_version
+  # `doctl kubernetes options versions` doesn't return anything if the linor k8s version isn't suported anymore.
+  # hardoding the version until we switch to Kubernetes 1.21
+  doks_version           = "1.20.15-do.1"
+  # doks_version           = data.digitalocean_kubernetes_versions.k8s.latest_version
   minimal_node_pool_size = "s-1vcpu-2gb" # Available sizes: `doctl compute size list`
 }
