6469650743b861dbb06dd3e69e53288f833158ba
smerle33 <smerle@cloudbees.com>
2022-04-20 11:22:52 +0200

    fix:lint

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ resource "random_string" "suffix" {
 }
 
 locals {
-  cluster_name           = lower("jenkins-infra-doks-${random_string.suffix.result}")
+  cluster_name = lower("jenkins-infra-doks-${random_string.suffix.result}")
   # `doctl kubernetes options versions` doesn't return anything if the minor k8s version isn't supported anymore, note it can fail the build.
   doks_version           = data.digitalocean_kubernetes_versions.k8s.latest_version
   minimal_node_pool_size = "s-1vcpu-2gb" # Available sizes: `doctl compute size list`
