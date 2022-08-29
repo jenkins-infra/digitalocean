5c75b19900fae4557fdbae02a7e0cc5f93ec9a5b
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-08-29 15:46:06 +0200

    chore: reactivate autoscaled node (revert 8fa48ed)

--- autoscaled-node-pool.tf
+++ autoscaled-node-pool.tf
@@ -1,16 +1,16 @@
-# resource "digitalocean_kubernetes_node_pool" "autoscaled-pool" {
-# cluster_id = digitalocean_kubernetes_cluster.doks_cluster.id
-# name       = "autoscaled-node-pool"
-# size       = var.autoscaled_node_pool_size
-# auto_scale = true
-# min_nodes  = 1
-# max_nodes  = var.autoscaled_node_pool_max_nodes
-# tags       = ["node-pool-autoscaled", local.cluster_name]
-# lifecycle {
-#   ignore_changes = [
-#     node_count,
-#     actual_node_count,
-#     nodes,
-#   ]
-# }
-# }
+resource "digitalocean_kubernetes_node_pool" "autoscaled-pool" {
+  cluster_id = digitalocean_kubernetes_cluster.doks_cluster.id
+  name       = "autoscaled-node-pool"
+  size       = var.autoscaled_node_pool_size
+  auto_scale = true
+  min_nodes  = 1
+  max_nodes  = var.autoscaled_node_pool_max_nodes
+  tags       = ["node-pool-autoscaled", local.cluster_name]
+  lifecycle {
+    ignore_changes = [
+      node_count,
+      actual_node_count,
+      nodes,
+    ]
+  }
+}
