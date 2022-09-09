0a82e6bfd7c78e5ee298e4b8242ee021d99f24e4
Hervé Le Meur <hlemeur@cloudbees.com>
2022-09-09 15:20:29 +0200

    set domain.id and remove implied dependency

--- dns.tf
+++ dns.tf
@@ -5,24 +5,18 @@ resource "digitalocean_domain" "top_level_domains" {
 
 resource "digitalocean_record" "a_records" {
   for_each = toset(var.top_level_domains)
-  domain   = each.value
+  domain   = each.value.id
   type     = "A"
   ttl      = 60
   name     = "@"
   value    = digitalocean_loadbalancer.ingress_load_balancer.ip
-  depends_on = [
-    digitalocean_domain.top_level_domains,
-  ]
 }
 
 resource "digitalocean_record" "cname_redirects" {
   for_each = toset(var.top_level_domains)
-  domain   = each.value
+  domain   = each.value.id
   type     = "CNAME"
   ttl      = 60
   name     = "www"
   value    = "@"
-  depends_on = [
-    digitalocean_domain.top_level_domains,
-  ]
 }
