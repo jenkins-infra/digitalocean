0949cc0081381150fa1f0d1cfd80f7dfdf06eade
Hervé Le Meur <hlemeur@cloudbees.com>
2022-09-09 16:19:36 +0200

    add repo.do.jenkins.io record

--- dns.tf
+++ dns.tf
@@ -9,3 +9,12 @@ resource "digitalocean_record" "a_record" {
   name   = "@"
   value  = digitalocean_loadbalancer.ingress_load_balancer.ip
 }
+
+# DNS record for repo.do.jenkins.io (https://github.com/jenkins-infra/helpdesk/issues/2752)
+resource "digitalocean_record" "cname_redirect" {
+  domain = digitalocean_domain.default.id
+  type   = "CNAME"
+  ttl    = 60
+  name   = "repo"
+  value  = "@"
+}
