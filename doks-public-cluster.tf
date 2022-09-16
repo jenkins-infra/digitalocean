3f789b80b7f80d3a2162013352218b86147920a4
Hervé Le Meur <hlemeur@cloudbees.com>
2022-09-16 17:48:28 +0200

    feat: public cluster To be used for public services like the Artifact Caching Proxy, in order to avoid putting credentials on the cluster dedicated to ci.jenkins.io agents. Fixes #66
    Ref: https://github.com/jenkins-infra/helpdesk/issues/3114

---
+++ doks-public-cluster.tf
@@ -0,0 +1,30 @@
+resource "digitalocean_kubernetes_cluster" "doks_public_cluster" {
+  name          = local.public_cluster_name
+  region        = var.region
+  version       = local.doks_version
+  auto_upgrade  = var.auto_upgrade
+  surge_upgrade = true
+  tags          = ["managed-by:terraform"]
+  lifecycle {
+    ignore_changes = [
+      updated_at,
+    ]
+  }
+
+  maintenance_policy {
+    start_time = var.maintenance_policy_start_time
+    day        = var.maintenance_policy_day
+  }
+
+  # Small node pool without autoscalling.
+  # As we can't scale to 0 with DO, we're setting up 2 node pools:
+  # - this one with the minimal size and no heavy usage
+  # - another beefy one with autoscalling enabled, see ./autoscaled-node-pool.tf
+  node_pool {
+    name       = "minimal-node-pool"
+    size       = local.minimal_node_pool_size
+    auto_scale = false
+    node_count = 1
+    tags       = ["node-pool-minimal", local.cluster_name]
+  }
+}
