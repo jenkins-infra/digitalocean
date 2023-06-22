6884698365cd8860f4a828104f55b2a35eb213c3
Damien Duportal <damien.duportal@gmail.com>
2023-06-22 17:36:38 +0200

    feat(doks) bump Kubernetes to 1.25.x (#119)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- doks-public-cluster.tf
+++ doks-public-cluster.tf
@@ -1,5 +1,5 @@
 data "digitalocean_kubernetes_versions" "doks-public" {
-  version_prefix = "1.24."
+  version_prefix = "1.25."
 }
 
 resource "digitalocean_kubernetes_cluster" "doks_public_cluster" {
