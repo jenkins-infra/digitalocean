9a6ab3709239996a3a07327c25a4bf721fdd4e08
Damien Duportal <damien.duportal@gmail.com>
2023-09-25 19:11:40 +0200

    feat: add a DNS zone with records for doks-public, archives(ipv4) and archives(ipv6)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ dns.tf
@@ -0,0 +1,26 @@
+# Child DNS Zone delegated from Azure
+# https://docs.digitalocean.com/products/networking/dns/getting-started/dns-registrars/
+resource "digitalocean_domain" "do_jenkins_io" {
+  name = "do.jenkins.io"
+}
+
+resource "digitalocean_record" "repo_ipv4" {
+  domain = digitalocean_domain.do_jenkins_io.id
+  type   = "A"
+  name   = "repo"
+  value  = data.digitalocean_loadbalancer.doks_public.ip
+}
+
+resource "digitalocean_record" "archives_ipv4" {
+  domain = digitalocean_domain.do_jenkins_io.id
+  type   = "A"
+  name   = "archives"
+  value  = digitalocean_droplet.archives_jenkins_io.ipv4_address
+}
+
+resource "digitalocean_record" "archives_ipv6" {
+  domain = digitalocean_domain.do_jenkins_io.id
+  type   = "AAAA"
+  name   = "archives"
+  value  = digitalocean_droplet.archives_jenkins_io.ipv6_address
+}
