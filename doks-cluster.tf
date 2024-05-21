ce0b13cbc4b038206a41cd71bb0044ff736a76f8
Damien Duportal <damien.duportal@gmail.com>
2024-05-21 14:17:57 +0200

    cleanup: empty doks and doks-public Kubernetes clusters as per https://github.com/jenkins-infra/helpdesk/issues/3954\#issuecomment-2119858778
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- doks-cluster.tf
+++ doks-cluster.tf
@@ -40,32 +40,3 @@ resource "digitalocean_kubernetes_node_pool" "autoscaled-pool" {
   max_nodes  = 50
   tags       = ["node-pool-autoscaled", local.cluster_name]
 }
-
-provider "kubernetes" {
-  alias                  = "doks"
-  host                   = digitalocean_kubernetes_cluster.doks_cluster.kube_config.0.host
-  cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.doks_cluster.kube_config.0.cluster_ca_certificate)
-  # Bootstrap requires to use the DigitalOcean API user as no service account or technical user are created in the cluster
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "doctl"
-    args = ["kubernetes", "cluster", "kubeconfig", "exec-credential",
-    "--version=v1beta1", digitalocean_kubernetes_cluster.doks_cluster.id]
-  }
-}
-
-# Configure the jenkins-infra/kubernetes-management admin service account
-module "doks_admin_sa" {
-  providers = {
-    kubernetes = kubernetes.doks
-  }
-  source                     = "./.shared-tools/terraform/modules/kubernetes-admin-sa"
-  cluster_name               = local.cluster_name
-  cluster_hostname           = digitalocean_kubernetes_cluster.doks_cluster.kube_config.0.host
-  cluster_ca_certificate_b64 = digitalocean_kubernetes_cluster.doks_cluster.kube_config.0.cluster_ca_certificate
-}
-
-output "kubeconfig_doks" {
-  sensitive = true
-  value     = module.doks_admin_sa.kubeconfig
-}
