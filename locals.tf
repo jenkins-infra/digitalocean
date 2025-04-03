8383f13111f0203f0c428da59e255aa7cc78da82
smerle33 <stephane.merle+jenkins@gmail.com>
2025-04-03 09:08:52 +0200

    tags

---
+++ locals.tf
@@ -0,0 +1,12 @@
+locals {
+  # Ref. https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/about-githubs-ip-addresses
+  # Only IPv4
+  # TODO track with updatecli
+  github_ips = {
+    webhooks = ["140.82.112.0/20", "143.55.64.0/20", "185.199.108.0/22", "192.30.252.0/22"]
+  }
+  default_tags = {
+    scope      = "terraform-managed"
+    repository = "jenkins-infra_digitalocean"
+  }
+}
