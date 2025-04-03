cb98fe7bc12957c0362b54a2891a15cf95fd54f1
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2025-04-03 18:34:11 +0200

    feat(puppet): create a new droptlet for puppet and rework the firewalls (#223)
    * feat(puppet): create a new droptlet for puppet and rework the firewalls
    
    * fmt
    
    * tags as strings

--- locals.tf
+++ locals.tf
@@ -1,4 +1,10 @@
 locals {
+  # Ref. https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/about-githubs-ip-addresses
+  # Only IPv4
+  # TODO track with updatecli
+  github_ips = {
+    webhooks = ["140.82.112.0/20", "143.55.64.0/20", "185.199.108.0/22", "192.30.252.0/22"]
+  }
   default_tags = {
     scope      = "terraform-managed"
     jenkins_infra_repository = "digitalocean"
