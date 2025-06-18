c602fb1b8692c6b68bdca857615673a48f1a3a99
Jaideep <79799016+jayfranco999@users.noreply.github.com>
2025-06-18 19:16:12 +0530

    Apply suggestions from code review
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- jay.training.tf
+++ jay.training.tf
@@ -3,18 +3,6 @@ resource "digitalocean_ssh_key" "jay_training" {
   public_key = file("ssh/id_jay_training.pub")
 }
 
-resource "digitalocean_volume" "jay_training_data" {
-  region                  = var.region
-  name                    = "jaytrainingdata" # Only lowercase alphanum
-  size                    = 750
-  initial_filesystem_type = "ext4"
-  description             = "Data disk for jay.training"
-}
-
-resource "digitalocean_volume_attachment" "jay_training_data" {
-  droplet_id = digitalocean_droplet.jay_training.id
-  volume_id  = digitalocean_volume.jay_training_data.id
-}
 
 resource "digitalocean_droplet" "jay_training" {
   image       = "ubuntu-22-04-x64"
@@ -25,6 +13,5 @@ resource "digitalocean_droplet" "jay_training" {
   ipv6        = true
   resize_disk = true
   ssh_keys    = [digitalocean_ssh_key.jay_training.fingerprint]
-  user_data   = templatefile("${path.root}/cloudinit.tftpl", { hostname = "jay.training.io" })
   tags        = concat([for key, value in local.default_tags : "${key}:${value}"])
 }
