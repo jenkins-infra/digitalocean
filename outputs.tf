6dad2596d6f0cdafece81bafa99325f75f84da31
jayfranco999 <jaideepfrancis999@gmail.com>
2025-06-18 22:55:27 +0530

    chore(outputs.tf): retrieve IP address of jay.training
    Signed-off-by: jayfranco999 <jaideepfrancis999@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -16,7 +16,21 @@ resource "local_file" "jenkins_infra_data_report" {
         "ipv6" = digitalocean_droplet.archives_jenkins_io.ipv6_address,
       },
     },
-
+    "jay.training" = {
+      "name_servers" = [
+        "ns1.digitalocean.com",
+        "ns2.digitalocean.com",
+        "ns3.digitalocean.com",
+      ],
+      "service_ips" = {
+        "ipv4" = digitalocean_droplet.jay_training.ipv4_address,
+        "ipv6" = digitalocean_droplet.jay_training.ipv6_address,
+      },
+      "outbound_ips" = {
+        "ipv4" = digitalocean_droplet.jay_training.ipv4_address,
+        "ipv6" = digitalocean_droplet.jay_training.ipv6_address,
+      },
+    },
   })
   filename = "${path.module}/jenkins-infra-data-reports/digitalocean.json"
 }
