9be8f8c2839305e1dbf1c0eeb88cb7f4acba848d
Damien Duportal <damien.duportal@gmail.com>
2025-07-31 08:32:09 +0200

    Update firewalls.tf (#248)

--- firewalls.tf
+++ firewalls.tf
@@ -58,9 +58,9 @@ resource "digitalocean_firewall" "archives" {
       # TODO: track with updatecli - https://reports.jenkins.io/jenkins-infra-data-reports/azure-net.json
       ["172.176.126.194"], # VPN outbound IP
       # TODO: track with updatecli - https://reports.jenkins.io/jenkins-infra-data-reports/azure-net.json
-      ["172.200.139.164", "128.24.89.148"], # Outbound IPv4 of the privatek8s-sponsorship.jenkins.io NAT gateway (release.ci agents, controller and infra.ci controller)
+      ["20.57.120.46", "52.179.141.53"], # Outbound IPv4 of the privatek8s cluster NAT gateway (release.ci agents, controller and infra.ci controller)
       # TODO: track with updatecli - https://reports.jenkins.io/jenkins-infra-data-reports/azure-net.json
-      ["20.122.14.108", "20.186.70.154"], # Outbound IPv4 of the infracijioagents-1-sponsorship NAT gateway (infra.ci agents)
+      ["172.210.200.59", "20.10.193.4"], # Outbound IPv4 of the infracijio agents NAT gateway
     ))
   }
 
