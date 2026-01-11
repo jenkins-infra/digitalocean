6ab50c9ef53570f0e103e14eeb5c48ab3feb5da0
Damien Duportal <damien.duportal@gmail.com>
2026-01-11 17:28:46 +0100

    cleanup(firewall) remove remnants of pkg.origin.jenkins.io VM (#276)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- locals.tf
+++ locals.tf
@@ -26,9 +26,5 @@ locals {
   # Tracked by 'updatecli' from the following source: https://reports.jenkins.io/jenkins-infra-data-reports/azure-net.json
   outbound_ips_infra_ci_jenkins_io = "20.57.120.46 52.179.141.53 172.210.200.59 20.10.193.4"
   # TODO: track with updatecli
-  inbound_ips_pkg_origin_jenkins_io = "52.202.51.185"
-  # TODO: track with updatecli
-  outbound_ips_pkg_origin_jenkins_io = "52.202.51.185"
-  # TODO: track with updatecli
   inbound_ips_ftp_osl_osuosl_org = "140.211.166.134 2605:bc80:3010::134"
 }
